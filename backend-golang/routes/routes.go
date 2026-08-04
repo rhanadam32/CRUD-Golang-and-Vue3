05c9cf8b8388964504b3e2ac4a33ac0ef4988099
Raihan Adam <raihanadam321@gmail.com>
2026-08-04 20:36:43 +0700

    feat: initialize backend project with Gin, GORM dependencies and API routing structure

--- backend-golang/routes/routes.go
+++ backend-golang/routes/routes.go
@@ -4,11 +4,19 @@ import (
 	"santri-koding-trial/backend-golang/controllers"
 	"santri-koding-trial/backend-golang/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
 	router.POST("/api/register", controllers.Register)
 	router.POST("/api/login", controllers.Login)
 	router.GET("/api/users", middlewares.AuthMiddleware(), controllers.FindUsers)
