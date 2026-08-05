3a7cfa558760f60340b6b0b89ac0fb15a61e7e35
Raihan Adam <raihanadam321@gmail.com>
2026-08-05 22:10:11 +0700

    feat: initialize frontend project with authentication views and routing support

--- frontend-vue-ts/src/App.vue
+++ frontend-vue-ts/src/App.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div class="container">
+        <router-link :to="{ name: 'home' }" class="navbar-brand">HOME</router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <!--- render router view -->
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
--- frontend-vue-ts/src/main.ts
+++ frontend-vue-ts/src/main.ts
@@ -4,8 +4,12 @@ import App from './App.vue';
 
 import { VueQueryPlugin } from '@tanstack/vue-query';
 
+import routes from './routes';
+
 const app = createApp(App);
 
 app.use(VueQueryPlugin);
+
+app.use(routes);
 
 app.mount('#app');
